c4fce0bc4eae624d02daf9e2a24f78ed73b4124e
97Chanuk <chanuk9531@daum.net>
2023-05-23 20:06:39 +0900

    Project-Setting
    프로젝트 시작

---
+++ src/test/java/com/example/linedemo/LineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
